972294d03add4089df69e6047ea3b15e13cd1bdf
Nathalia <naty@gmail.com>
2017-05-29 17:22:02 -0400

    tipo de dato

--- SGS/src/org/openxava/sgs/model/Cliente.java
+++ SGS/src/org/openxava/sgs/model/Cliente.java
@@ -1,5 +1,7 @@
 package org.openxava.sgs.model;
 
+import java.math.*;
+
 import javax.persistence.*;
 import org.openxava.annotations.*;
 import org.openxava.model.*;
@@ -18,7 +20,7 @@ public class Cliente {
 	public enum tipodoc { Cédula, Ruc }
 	
 	@Id @Column(length=40) @Required
-	private String nroDocumento;
+	private BigDecimal nroDocumento;
 	
 	@Column(length=40) @Required
 	private String razonSocial;
@@ -32,20 +34,20 @@ public class Cliente {
 		this.documento = documento;
 	}
 
-	public String getNroDocumento() {
-		return nroDocumento;
-	}
-
-	public void setNroDocumento(String nroDocumento) {
-		this.nroDocumento = nroDocumento;
-	}
-
 	public String getRazonSocial() {
 		return razonSocial;
 	}
 
 	public void setRazonSocial(String razonSocial) {
 		this.razonSocial = razonSocial;
+	}
+
+	public BigDecimal getNroDocumento() {
+		return nroDocumento;
+	}
+
+	public void setNroDocumento(BigDecimal nroDocumento) {
+		this.nroDocumento = nroDocumento;
 	}
 	
 	
